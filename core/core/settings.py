56bdc3bb2b1b62df6fc9f5b7d682e51b81ca298d
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-05-14 12:45:01 +0400

    End

--- core/core/settings.py
+++ core/core/settings.py
@@ -80,15 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': os.environ.get('MYSQL_DB_NAME'),
-		'USER': os.environ.get('MYSQL_USER'),
-		'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-		'HOST':os.environ.get('MYSQL_HOST'),
-		'PORT':os.environ.get('MYSQL_PORT'),
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
